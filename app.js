f96e7f092d0c245a077dc1b5415fd56c3defaa58
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-03-31 21:48:40 +0600

    ver 1.0 working

--- app.js
+++ app.js
@@ -1,6 +1,96 @@
 const numberBtns = document.querySelectorAll('.number-button');
+const operationBtns = document.querySelectorAll('.operation-button');
 const lowerField = document.querySelector('.current-operation');
 const upperField = document.querySelector('.last-operation');
+
+const resetBtn = document.querySelector('#reset-button');
+const deleteBtn = document.querySelector('#delete-button');
+const equationBtn = document.querySelector('#equation-button');
+
+let currentOperator = null;
+let firstNum = 0;
+let secondNum = 0;
+
+resetBtn.addEventListener('click',() =>{
+  lowerField.textContent = '';
+  upperField.textContent = '';
+  firstNum = 0;
+  secondNum = 0;
+});
+
+deleteBtn.addEventListener('click',()=> deleteNumber());
+
+numberBtns.forEach(btn => {
+  btn.addEventListener('click',() => addNumber(btn.textContent));
+});
+
+operationBtns.forEach(btn =>{
+  btn.addEventListener('click',() => operateButton(btn.textContent));
+});
+
+equationBtn.addEventListener('click',()=> evaluate());
+
+document.addEventListener('keydown',(e)=>{
+  const operations = ['/','+','-','*','x'];
+  if(operations.includes(e.key))
+  operateButton(e.key);
+  if(e.key >= 0 && e.key <= 9 || e.key=='.')
+  addNumber(e.key);
+  if(e.key == 'Backspace')
+  deleteNumber();
+  if(e.key == '=' || e.key =='Enter')
+  evaluate();
+})
+
+function operateButton(button){
+    currentOperator = button;
+    firstNum = lowerField.textContent;
+    upperField.style.color="black";
+    upperField.textContent=`${lowerField.textContent} ${button}`;
+    lowerField.textContent='';
+}
+
+function evaluate(){
+  upperField.textContent='';
+  secondNum = lowerField.textContent;
+  lowerField.textContent = operate(currentOperator,firstNum,secondNum);
+  firstNum = 0;
+  secondNum = 0;
+}
+
+function addNumber(number){
+  if(lowerField.textContent.length<13)
+  lowerField.textContent += number;
+}
+
+function deleteNumber(){
+  lowerField.textContent = lowerField.textContent.slice(0,-1);
+}
+
+function operate(currentOperator, firstNum, secondNum){
+  firstNum = Number(firstNum);
+  secondNum = Number(secondNum);
+
+  switch(currentOperator){
+    case '/':
+      if(secondNum==0)
+      return "Can't do that!";
+     return divide(firstNum,secondNum);
+      break;
+    case 'x':
+      return multiply(firstNum,secondNum);
+      break;
+    case '+':
+      return add(firstNum,secondNum);
+      break;
+    case '-':
+     return substract(firstNum,secondNum);
+      break;
+    default:
+      return null;
+  };
+};
+
 
 function add(a,b) {
   return a + b;
@@ -17,9 +107,3 @@ function multiply(a,b){
 function divide(a, b) {
   return a / b
 }
-
-numberBtns.forEach(btn => {
-  btn.addEventListener('click',()=>{
-    lowerField.innerHTML += btn.innerHTML;
-  });
-});
